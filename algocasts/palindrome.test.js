69476e1e53988a88b3196f45a35d0fa93ce34a03
thinktwice13 <marioo.krajacic@gmail.com>
2020-11-10 14:25:45 +0100

    Update check palnidrome

--- algocasts/palindrome.test.js
+++ algocasts/palindrome.test.js
@@ -1,13 +1,16 @@
 const { a, b } = require('./palindrome')
 
-it('A', () => {
-  expect(a('abba')).toBe(true)
-  expect(a('abcba')).toBe(true)
-  expect(a('abcd')).toBe(false)
-})
 
-it('B', () => {
-  expect(b('abba')).toBe(true)
-  expect(b('abcba')).toBe(true)
-  expect(b('abcd')).toBe(false)
+describe('check palnidrome', () => {
+  test('A', () => {
+    expect(a('abba')).toBe(true)
+    expect(a('abcba')).toBe(true)
+    expect(a('abcd')).toBe(false)
+  })
+
+  test('B', () => {
+    expect(b('abba')).toBe(true)
+    expect(b('abcba')).toBe(true)
+    expect(b('abcd')).toBe(false)
+  })
 })
