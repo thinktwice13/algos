23a01f2895847eb755ac95335e2bef60949cbdcb
thinktwice13 <marioo.krajacic@gmail.com>
2020-11-12 15:23:54 +0100

    Add memoization to fibonacci

--- algocasts/fibonacci.js
+++ algocasts/fibonacci.js
@@ -1,7 +1,22 @@
 //Print out n-th entry in fibonacci series
-const fib = n => 
-  n < 2
-    ? n
-    : fib(n - 1) + fib(n - 2)
+const memo = fn => {
+  const cache = {}
+
+  return (...args) => {
+    // if set of args exists in cache, return
+    // otherwise calculate and save for future reference
+    if (!cache[args]) {
+      cache[args] = fn.apply(this, args)
+    }
+    return cache[args]
+  }
+}
+
+const fib = memo(
+  n =>
+    n < 2
+      ? n
+      : fib(n - 1) + fib(n - 2)
+)
 
 module.exports = fib
--- algocasts/fibonacci.test.js
+++ algocasts/fibonacci.test.js
@@ -4,4 +4,7 @@ test('fibonacci', () => {
   expect(fn(4)).toEqual(3)
   expect(fn(6)).toEqual(8)
   expect(fn(9)).toEqual(34)
+  expect(fn(15)).toEqual(610)
+  expect(fn(28)).toEqual(317811) // O(2^n)
+  expect(fn(40)).toEqual(102334155) // O(2^n)
 })
