7dce0163edc79909b008c828703ebd5a46d170ee
thinktwice13 <marioo.krajacic@gmail.com>
2020-11-17 11:54:36 +0100

    Update LinkedLisst midpoint

--- algocasts/data structures/midpoint.js
+++ algocasts/data structures/midpoint.js
@@ -1,18 +1,19 @@
 // Return middle node of a linkedlist. Don't use counters and .size()
 
 // Solution: Use slow+fast itetaros. Compare to get result
-const midpoint = (ll, s = ll.getFirst(), f = ll.getFirst()) => {
-  // let s = ll.getFirst()
-  // let f = ll.getFirst()
-  // while (f.next && f.next.next) { // works because the exact reference of f does not matter!
-  //   f = f.next.next
-  //   s = s.next
-  // }
-  // return s
-
-  return !f.next || !f.next.next 
-    ? s 
+const midpoint = (ll, s = ll.getFirst(), f = ll.getFirst()) => 
+  !f.next || !f.next.next
+    ? s
     : midpoint(ll, s.next, f.next.next)
-}
+
+
+// Alt solution
+// let s = ll.getFirst()
+// let f = ll.getFirst()
+// while (f.next && f.next.next) { // works because the exact reference of f does not matter!
+//   f = f.next.next
+//   s = s.next
+// }
+// return s
 
 module.exports = midpoint
