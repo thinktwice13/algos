de432e3e4e7d34e030fead4820efd00475db4516
thinktwice13 <marioo.krajacic@gmail.com>
2020-11-18 11:50:43 +0100

    Update Tree levelWidth

--- algocasts/trees/level-width.js
+++ algocasts/trees/level-width.js
@@ -1,11 +1,9 @@
-const lw = (r, l = 0, arr = [1]) => {
+const lw = (r, level = 0, arr = [1]) => {
   if (!r.children) return arr;
-  l++
+  level++
   for (let child of r.children) {
-    if (arr[l]) arr[l]++
-    else arr[l] = 1
-    
-    lw(child, l, arr) // Do not return!
+    arr[level] = (arr[level] || 0) + 1
+    lw(child, level, arr)
   }
   return arr;
 }
