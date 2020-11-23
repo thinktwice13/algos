342217240f06362e1c89a16876a96f5489dd9a2e
thinktwice13 <marioo.krajacic@gmail.com>
2020-11-23 11:31:57 +0100

    Update BST validation solution

--- algocasts/trees/validate-bst.js
+++ algocasts/trees/validate-bst.js
@@ -1,13 +1,18 @@
 // Validate BST tree
 
 const validate = (node, min = null, max = null) => {
-  if (max !== null && node.data > max) return false
-  if (min !== null && node.data < min) return false
+  // if (max !== null && node.data > max) return false
+  // if (min !== null && node.data < min) return false
 
-  // return false if validation of left/right nodes fails
-  if (node.left && !validate(node.left, min, node.data)) return false
-  if (node.right && !validate(node.right, node.data, max)) return false
+  // // return false if validation of left/right nodes fails
+  // if (node.left && !validate(node.left, min, node.data)) return false
+  // if (node.right && !validate(node.right, node.data, max)) return false
 
+  // return true
+
+  if (min && node.data < min || max && node.data > max) return false
+  if (node.left) return validate(node.left, min, node.data)
+  if (node.right) return validate(node.right, node.data, max)
   return true
 }
 
