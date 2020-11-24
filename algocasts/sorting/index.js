08a049e7a3586bff0425d5361b31676da66ae653
thinktwice13 <marioo.krajacic@gmail.com>
2020-11-24 08:57:47 +0100

    Add SelectionSort

--- algocasts/sorting/index.js
+++ algocasts/sorting/index.js
@@ -17,7 +17,22 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  // Kep the record of the lowest element found and only swap at the end of the loop
 
+  for(let i = 0; i < arr.length; i++) {
+    let indexOfMin = i // Assume i is the lowest
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) indexOfMin = j
+    }
+    // swap currently assumed lowest and new found lowest
+    if (arr[i] !== arr[indexOfMin]) {
+      let tmp = arr[i]
+      arr[i] = arr[indexOfMin]
+      arr[indexOfMin] = tmp
+    }
+  }
+  return arr
 }
 
 function mergeSort(arr) {
