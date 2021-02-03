3afa09fcb975687b54e441682c696e95c8017f7d
thinktwice13 <marioo.krajacic@gmail.com>
2021-02-03 12:44:49 +0100

    Add CTCI ch8q1

---
+++ CtCI/ch8/q1.js
@@ -0,0 +1,43 @@
+// In how many ways can a child run up a taircase of size n? Possible steps: 1, 2 or 3
+
+const fn = n => {
+  if (n < 0) return 0;
+  if (n < 1) return 1;
+
+  // Return sum of possible combinations O(3^n) // Hint CTCI puzzle with poison bottles: 1000 bottles on 10 test strips = 2^10
+  return fn(n - 1) + fn(n - 2) + fn(n - 3);
+}
+
+const withMemo = (n, memo = []) => {
+  if (n < 0) return 0;
+  if (n < 1) return 1;
+
+  if (!memo[n]) {
+    memo[n] = withMemo(n - 1, memo) + withMemo(n - 2, memo) + withMemo(n - 3, memo);
+  }
+
+  return memo[n];
+}
+
+const rev = n => {
+  if (n < 0) return 0;
+  if (n == 0) return 1;
+
+  let prev1 = 1
+  let prev2 = 0
+  let prev3 = 0
+
+  for (let nn = 1; nn < n; nn++) {
+    const next = prev1 + prev2 + prev3
+    prev3 = prev2
+    prev2 = prev1
+    prev1 = next
+  }
+
+  return prev1 + prev2 + prev3
+}
+
+// Test
+const n = 15
+const res = rev(n)
+console.log(res)
