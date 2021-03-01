6bbae3b76e4c9df5f3d5ddf5418be09d3fec8f13
thinktwice13 <marioo.krajacic@gmail.com>
2021-03-01 22:27:52 +0100

    Add Codility 10 MaxPerimeterRectangle

---
+++ codility/10-MinPerimeterRectangle.js
@@ -0,0 +1,27 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(N) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  const dividerMax = Math.floor(Math.sqrt(N))
+
+  for (let d1 = dividerMax; d1 > 1; d1--) {
+    // Dividers are valid of they are integers and if their product is N
+    const d2 = N / d1
+    const isInteger = d2 % 1 === 0
+    if (isInteger) {
+      const productEqualsN = d1 * d2 === N
+      if (productEqualsN) {
+        const perimeter = 2 * (d1 + d2)
+        return perimeter
+      } 
+    }
+  }
+
+  // If everything else fails, it is a prime number
+  return N * 2 + 2
+}
+
+// TEST
+const n = 2
+const r = solution(n)
