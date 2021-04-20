56341c7666939dee519ddb5050bd7fe016a750ed
thinktwice13 <marioo.krajacic@gmail.com>
2021-04-20 11:00:06 +0200

    Add leetcode dir

---
+++ leetcode/twoSum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const a = nums[i];
+
+    const b = target - a;
+    if (map.hasOwnProperty(b)) return [map[b], i];
+    else map[a] = i;
+  }
+};
