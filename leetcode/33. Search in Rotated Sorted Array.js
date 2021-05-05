ecc0d9bbf1469a3e04c54a8d4faaf5d14525fc8a
thinktwice13 <marioo.krajacic@gmail.com>
2021-05-05 10:33:26 +0200

    Add leetcode 33

---
+++ leetcode/33. Search in Rotated Sorted Array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    // is target found
+    if (nums[mid] === target) return mid;
+
+    // is left sorted?
+    if (nums[left] <= nums[mid]) {
+      // is target left?
+      if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+      else left = mid + 1;
+    }
+    // right sorted
+    else {
+      // is target right?
+      if (target > nums[mid] && target <= nums[right]) left = mid + 1;
+      else right = mid - 1;
+    }
+  }
+
+  return -1;
+};
