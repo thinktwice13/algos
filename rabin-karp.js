b80d3071f979957f12bc4cede9d2786c8f918eee
thinktwice13 <marioo.krajacic@gmail.com>
2021-05-11 12:47:08 +0200

    Add rabin-karp

---
+++ rabin-karp.js
@@ -0,0 +1,39 @@
+const hash = (s) =>
+  s.split('').reduce((h, b) => {
+    h = (h << 5) - h + b.charCodeAt(0);
+    return h & h;
+  }, 0);
+
+const areEqual = (s1, s2) => {
+  if (s1 !== s2) {
+    return false;
+  }
+  for (i = 0; i < s1.length; i++) {
+    if (s1[i] !== s2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/**
+ * find all occurrences of a substring in a given text based on hash values
+ */
+const rk = (text, substr) => {
+  // Get hash of the entire text
+  const ssHash = hash(substr);
+  let matches = 0;
+
+  // Iterate text in search for substring  and ocmpare hashes. If hashes match, ocmpare actual substring
+  for (let i = 0; i <= text.length - substr.length; i++) {
+    const currentSubstring = text.slice(i, i + substr.length);
+    const currHash = hash(currentSubstring);
+
+    currHash === ssHash && areEqual(substr, currentSubstring) && ++matches;
+  }
+
+  return matches;
+};
+
+const str = 'aaaaaabbaaaaaabbaaaabb';
+const m = rk(str, 'bba');
