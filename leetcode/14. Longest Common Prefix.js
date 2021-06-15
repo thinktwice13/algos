7c56e1ae717e408c26524af898290d235cab9493
thinktwice13 <marioo.krajacic@gmail.com>
2021-06-15 13:22:20 +0200

    Update leetcode

---
+++ leetcode/14. Longest Common Prefix.js
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+var longestCommonPrefixA = function (strs) {
+  let prefix = strs[0];
+  if (!prefix.length) return '';
+
+  for (let i = 1; i < strs.length; i++) {
+    const s = strs[i];
+    if (!s.length) return '';
+    let pre = '';
+    let ii = 0;
+    while (s[ii] === prefix[ii] && ii <= s.length) {
+      pre += s[ii];
+      ii++;
+    }
+
+    if (pre.length < prefix.length) prefix = pre;
+  }
+
+  return prefix;
+};
+
+var longestCommonPrefixB = function (strs) {
+  strs.sort();
+
+  // Compare first/last
+  const [first, last] = [strs[0], strs[strs.length - 1]];
+  if (last.startsWith(first)) return first;
+
+  let pos = 0;
+  let prefix = '';
+  while (first[pos] === last[pos] && pos < first.length) {
+    prefix += first[pos];
+    pos++;
+  }
+  return prefix;
+};
+
+const strs = ['flower', 'flower', 'flower', 'flowe', 'flight'];
+longestCommonPrefixA(strs);
+longestCommonPrefixB(strs);
