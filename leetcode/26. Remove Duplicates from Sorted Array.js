f97aaf2d92892ce64fa60e98329ebc79a2eec739
thinktwice13 <marioo.krajacic@gmail.com>
2021-06-21 14:15:44 +0200

    leetcodeUpdate

--- leetcode/26. Remove Duplicates from Sorted Array.js
+++ leetcode/26. Remove Duplicates from Sorted Array.js
@@ -5,14 +5,21 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  let i = 1;
-  while (i < nums.length) {
-    if (nums[i] === nums[i - 1]) nums.splice(i, 1);
-    else i++;
+  let i = 0;
+
+  // Start second pointer at i + 1: The first possible duplicate
+  for (let ii = 1; ii < nums.length; ii++) {
+    // For every distinct num found, edit next i
+    if (nums[ii] !== nums[i]) {
+      nums[++i] = nums[ii];
+    }
   }
 
-  return nums;
+  nums;
+
+  return ++i; // So far, i was index, return coutn of distinct nums
 };
 
-const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+const nums = [1, 1, 2];
 removeDuplicates(nums);
