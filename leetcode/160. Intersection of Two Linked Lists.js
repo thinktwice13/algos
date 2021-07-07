981bfedb4360afa04918135fcdfb80aca9d31c7e
thinktwice13 <marioo.krajacic@gmail.com>
2021-07-07 13:51:34 +0200

    Update leetcode

---
+++ leetcode/160. Intersection of Two Linked Lists.js
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  const memo = new Set();
+
+  let a = headA;
+  while (a) {
+    memo.add(a);
+    a = a.next;
+  }
+
+  let b = headB;
+  while (b) {
+    if (memo.has(b)) return b;
+    b = b.next;
+  }
+
+  return null;
+};
+
+// O1 space solution: To avoid hashing visited nodes, conect llists to make a cycle
+var getIntersectionNode = function (headA, headB) {
+  if (!headA || !headB) return null;
+  let [a, b] = [headA, headB];
+  while (a !== b) {
+    // a = a?.next ?? headB;
+    // b = b?.next ?? headA;
+    a = a === null ? headB : a.next;
+    b = b === null ? headA : b.next;
+  }
+
+  return a; // || b
+};
