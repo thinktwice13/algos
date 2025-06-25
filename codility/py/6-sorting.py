c665cc6ecafe3d92ef31861c0b27ff423bfdcfcf
Mario Krajacic <marioo.krajacic@gmail.com>
2025-06-25 09:55:21 +0200

    Update Codility

--- codility/py/6-sorting.py
+++ codility/py/6-sorting.py
@@ -26,3 +26,30 @@ assert max_product_of_three([-4, -6, 3, 4, 5]) == 120
 assert max_product_of_three([-4, -6, 3, 4, 5, 10]) == 240
 assert max_product_of_three([-4, -6, 3, 4, 5, 10, 11]) == 550
 assert max_product_of_three([-5, -4, -3, -2, 3]) == 60
+
+def triangle(N):
+    """
+    In a sorted array P < Q < R always are true P+R>Q and Q+R>P. Only need to check for non-obvious case P+Q>R (Find sum of any two consecutive nums bigger than the following one)
+    Endge cases: negative nums, len N < 3, in other languages also overflow
+    :param N:
+    :return:
+    """
+
+    if len(N) < 3:
+        return 0
+
+    N.sort()
+    # In an example 1 2 5 8 10 20, going from 2 to 10 is enough for the check
+    for i in range(1,len(N)-1):
+        if (N[i]+N[i-1]) > N[i+1]:
+            return 1
+    return 0
+
+
+assert triangle([10, 2, 5, 1, 8, 20]) == 1
+assert triangle([10, 50, 5, 1]) == 0
+assert triangle([]) == 0
+assert triangle([1, 2]) == 0
+assert triangle([5, 5, 5]) == 1
+assert triangle([-5, -3, -1, 2, 8]) == 0
+assert triangle([2147483647, 2147483647, 2147483647]) == 1
